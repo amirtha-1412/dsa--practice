12a8f9f3e179815f0ea2ce362444ddee52c51f15
Amirtha Varshni M <kit27.am006@gmail.com>
2025-12-17 14:15:35 +0530

    Implement maxProfit method for stock prices

--- Arrays/LC121_BestTimeToBuyAndSellStock.java
+++ Arrays/LC121_BestTimeToBuyAndSellStock.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int maxProfit(int[] prices) {
+        int m1=prices[0];
+        int m2=0;
+        for (int i=1;i<prices.length;i++) {
+            if (prices[i]<m1) {
+                m1=prices[i];
+            } else {
+                int profit=prices[i]-m1;
+                m2=Math.max(m2,profit);
+            }
+        }
+        return m2;
+    }
+}
